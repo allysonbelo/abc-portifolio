53669d8ceb5248709c40f22e2a87cd7e187bbf0b
allysonbelo <allysonbelo@gmail.com>
2023-04-01 08:42:02 -0300

    Config: Configuração incial para inciar o desenvolvimento

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <div class="full-wrapper">
+        <header>
+            <div class="wrapper">
+                Header
+            </div>
+        </header>
