3827934f4bbcf0638e757e506ff4d69c8bba3d90
allysonbelo <allysonbelo@gmail.com>
2023-04-18 19:59:55 -0300

    Refact: Código refatorado, adicionado partes, e script para menu dropdown

---
+++ parts/content-banner.php
@@ -0,0 +1,10 @@
+<section class="wrapper">
+    <div class="banner">
+        <div class="banner__social">
+            Social
+        </div>
+        <div class="banner__wrapper">
+            Content
+        </div>
+    </div>
+</section>
