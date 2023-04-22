89226b1b271b813ff5ea7486d510a211deebf824
allysonbelo <allysonbelo@gmail.com>
2023-04-22 18:31:26 -0300

    Style: Ajustes no background do hero

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const themeToggle = document.getElementById("theme-toggle");
-    themeToggle.addEventListener("click", function() {
-        const body = document.body;
-        body.classList.toggle("light-mode");
-        themeToggle.classList.toggle("light-mode");
-    });
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     const themeToggle = document.getElementById("theme-toggle");
+//     themeToggle.addEventListener("click", function() {
+//         const body = document.body;
+//         body.classList.toggle("light-mode");
+//         themeToggle.classList.toggle("light-mode");
+//     });
+// });
