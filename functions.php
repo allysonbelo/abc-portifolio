190da8b76f7b067d78321db4341c7c7552cba6f9
allysonbelo <allysonbelo@gmail.com>
2023-05-04 20:32:01 -0300

    Add: Adicionado arquivo de css e função no functions.php

--- functions.php
+++ functions.php
@@ -6,9 +6,10 @@ function abcdev_load_scripts()
 {
     //Styles - Start
     wp_enqueue_style('abcdev-reset', get_template_directory_uri() . '/styles/reset.css', array(), filemtime(get_template_directory() . '/styles/reset.css'), 'all');
+    wp_enqueue_style('abcdev-base', get_template_directory_uri() . '/styles/base.css', array(), filemtime(get_template_directory() . '/styles/base.css'), 'all');
     wp_enqueue_style('abcdev-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'), 'all');
     wp_enqueue_style('abcdev-header', get_template_directory_uri() . '/styles/header.css', array(), filemtime(get_template_directory() . '/styles/header.css'), 'all');
-    wp_enqueue_style('abcdev-base', get_template_directory_uri() . '/styles/base.css', array(), filemtime(get_template_directory() . '/styles/base.css'), 'all');
+    wp_enqueue_style('abcdev-index', get_template_directory_uri() . '/styles/index.css', array(), filemtime(get_template_directory() . '/styles/index.css'), 'all');
     wp_enqueue_style('abcdev-content-hero', get_template_directory_uri() . '/styles/content-hero.css', array(), filemtime(get_template_directory() . '/styles/content-hero.css'), 'all');
     wp_enqueue_style('abcdev-dark-mode', get_template_directory_uri() . '/styles/dark-mode.css', array(), filemtime(get_template_directory() . '/styles/dark-mode.css'), 'all');
     wp_enqueue_style('abcdev-single', get_template_directory_uri() . '/styles/single.css', array(), filemtime(get_template_directory() . '/styles/single.css'), 'all');
--- index.php
+++ index.php
@@ -1,7 +1,25 @@
 <?php get_header(); ?>
 <main>
     <div class="wrapper">
-        Main
+        <div class="blog__container">
+
+            <?php if (have_posts()) :
+                while (have_posts()) : the_post(); ?>
+                    <article>
+                        <h2><?php the_title(); ?></h2>
+                        <div class="meta-info">
+                            <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+                            <p>Categories: <?php the_category(' '); ?></p>
+                            <p>Tags: <?php the_tags('', ', '); ?></p>
+                        </div>
+                        <?php the_content(); ?>
+                    </article>
+                <?php endwhile;
+            else : ?>
+                <p>Nothing yet to be displayed!</p>
+            <?php endif; ?>
+
+        </div>
     </div>
 </main>
 <?php get_footer(); ?>
