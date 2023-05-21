021ff6d50a1a53b2852892f36e5ec7d3702d5b9d
allysonbelo <allysonbelo@gmail.com>
2023-05-21 16:27:06 -0300

    Feat: seção sobre mim finalizada

--- parts/content-about.php
+++ parts/content-about.php
@@ -1,1 +1,25 @@
-<h2>About me</h2>
+<div class="wrapper">
+    <div class="about">
+        <h2 id="about" class="about__title">About me</h2>
+        <div class="about__container">
+            <div class="about__image">
+                <img src="<?php echo get_template_directory_uri() . '/img/profile.jpg' ?>" alt="">
+            </div>
+            <div class="about__content">
+                <h3 class="about__content--title">About me</h3>
+                <p class="about__content--text">
+                    I think i am a good humoured, very responsible, hard working and emotional person. I like creativity and appreciate this treit in others.
+
+                    I think i am a good humoured, very responsible, hard working and emotional person. I like creativity and appreciate this treit in others. I like creativity and appreciate this treit in others.
+                </p>
+                <h4 class="about__content--subtitle">My favorite things: </h4>
+                <ul class="about__content--list">
+                    <li>Video games</li>
+                    <li>Dogs</li>
+                    <li>Travel</li>
+                    <li>Nature</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
