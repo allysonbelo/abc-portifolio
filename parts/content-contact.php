3a0a7e25c16ef9568d9ed2ee4c05f251494039c8
allysonbelo <allysonbelo@gmail.com>
2023-05-23 21:06:01 -0300

    Add: Seção contact content finalizado

--- parts/content-contact.php
+++ parts/content-contact.php
@@ -1,5 +1,45 @@
 <div class="wrapper">
-    <section class="hero">
+    <section id="contact" class="contact">
+        <div class="contact__content">
+            <h2 class="contact__content--title">Contact</h2>
+            <p class="contact__content--text">
+                I think i am a good humoured, very responsible, hard working and emotional person. I like creativity and appreciate this treit in others. I like creativity and appreciate this treit in others.
+            </p>
+            <div class="contact__info">
+                <h3 class="contact__info--title">Address: </h3>
+                <span class="contact__info--text">Av. Presidente Café Filho</span>
+            </div>
+            <div class="contact__info">
+                <h3 class="contact__info--title">Phone: </h3>
+                <span class="contact__info--text">+55 83 99822 9897</span>
+            </div>
+            <div class="contact__info">
+                <h3 class="contact__info--title">E-mail: </h3>
+                <span class="contact__info--text">allysonbelo@gmail.com</span>
+            </div>
+        </div>
 
+        <div class="contact__form">
+            <h2 class="contact__content--title">Contact Form</h2>
+            <form class="contact__form--form" action="#">
+                <label>
+                    Your name
+                    <input type="text">
+                </label>
+                <label>
+                    Your phone
+                    <input type="text">
+                </label>
+                <label>
+                    Your e-mail
+                    <input type="text">
+                </label>
+                <label>
+                    Message
+                    <input type="text">
+                </label>
+                <button class="hero__content--button">Send</button>
+            </form>
+        </div>
     </section>
 </div>
